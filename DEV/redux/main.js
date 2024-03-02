f22d75ed8f188331cbf348c36893386dbb6ca08c
vishalBhangare <vishalbhangare2@gmail.com>
2024-03-02 11:26:37 +0530

    custom middleware

--- DEV/redux/main.js
+++ DEV/redux/main.js
@@ -6,15 +6,8 @@ const unsubscribe = store.subscribe(() => {
   console.log("store changed!", store.getState())
 })
 
-store.dispatch(todoAdded({ title: "title1" }))
-store.dispatch(todoAdded({ title: "title2" }))
-store.dispatch(todoAdded({ title: "title3" }))
-store.dispatch(todoCompleted({ id: 1 }))
-// unsubscribe();
-store.dispatch(userAdded({ name: "vishal" }))
-// store.dispatch(todoRemoved({ id: 1 }))
-store.dispatch(todoAssignedToUser({ todoId: 2, userId: 1 }))
-
-const a = getPendingTodos(store.getState());
-const b = getPendingTodos(store.getState());
-console.log(getTodosByUsers(2)(store.getState()))
+store.dispatch({
+  type: "error", payload: {
+    message: "hello",
+  }
+})
--- DEV/redux/src/configureStore.js
+++ DEV/redux/src/configureStore.js
@@ -1,10 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit'
 import reducer from './store/reducer'
-
-
+import logger from './store/middleware/logger'
+import toast from './store/middleware/toast'
 
 export default function () {
   return configureStore({
-    reducer
+    reducer,
+    middleware: getDefaultEnhancers => getDefaultEnhancers({ thunk: true }).concat(
+      logger("console"), toast
+    )
   })
 }
+
+// use applymiddleware func while creating store using createStore from redux
